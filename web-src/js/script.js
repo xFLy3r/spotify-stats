ad13e90e3d630e71b2543f858add6b1dcbea9a2d
xFLy3r <datus3r@gmail.com>
2017-11-06 16:41:27 +0200

    Fixed js file

--- web-src/js/script.js
+++ web-src/js/script.js
@@ -1,34 +1,23 @@
-$.ajax({
-    url: window.location.href + "statuses",
-    async: false,
-    dataType: 'json',
-    success: function (json) {
-        myData = json;
-    }
-});
-
-var data = {
-    labels: [
-        'Free',
-        'Premium'
-    ],
-    datasets: [{
-        data: [myData['countOfFree'], myData['countOfPremium']],
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)'
+function drawChart(myData) {
+    var data = {
+        labels: [
+            'Free',
+            'Premium'
         ],
-        borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)'
-        ],
-        borderWidth: 1
-    }]
+        datasets: [{
+            data: [myData['countOfFree'], myData['countOfPremium']],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
 
-};
-
-
-$(function() {
+    };
     var ctx = document.getElementById("test").getContext("2d");
     var test = new Chart(ctx,{
         type: 'pie',
@@ -42,5 +31,14 @@ $(function() {
             }
         }
     });
+}
+
+$.ajax({
+    url: window.location.href + 'statuses',
+    dataType: 'json',
+    success: function (json) {
+        drawChart(json);
+    }
 });
+
 
